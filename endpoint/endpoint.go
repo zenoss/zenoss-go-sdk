8d1f438dc60a042f1cd9d1e7fdba7b235fdb3db1
Vladyslav Tatarin <vtatarin@zenoss.com>
2024-08-28 15:34:57 +0300

    reuse connection

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -272,9 +272,7 @@ func New(config Config) (*Endpoint, error) {
 		// Dial doesn't block by default. So no error can actually occur.
 		conn, _ := grpc.Dial(config.Address, dialOptions...)
 		client = data_receiver.NewDataReceiverServiceClient(conn)
-
-		regconn, _ := grpc.Dial(config.Address, dialOptions...)
-		regclient = data_registry.NewDataRegistryServiceClient(regconn)
+		regclient = data_registry.NewDataRegistryServiceClient(conn)
 	}
 
 	var cache *ttlcache.Cache[string, MetricIDNameAndHash]
