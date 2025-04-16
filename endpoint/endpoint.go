aa2f022105cd2faeb451013c430e6a00345816e9
Iryna Kostashchuk <kostashchuk.irina@gmail.com>
2025-04-16 15:21:52 +0300

    spell error

--- endpoint/endpoint.go
+++ endpoint/endpoint.go
@@ -566,7 +566,7 @@ func (e *Endpoint) registerMetrics(ctx context.Context, metrics []*data_receiver
 	}
 
 	if len(failedMetrics) > 0 {
-		// retrying to register failed metrics from batchg
+		// retrying to register failed metrics from batch
 		expBoff.Reset()
 		metricsToRetry := extractMetrics(failedMetrics)
 		retryResponse, err := backoff.Retry(
